52b41fb9d53252efc109b42a09f5953d3eb96192
sameer sainii <justanid2006@gmail.com>
2026-06-28 22:26:53 +0530

    implemented crud

--- shortener/urls.py
+++ shortener/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
-from .views import Shorturlview
-urlpatterns=[path("shorten/",Shorturlview.as_view(),name="shorten-url"),]
+from .views import Shorturlview,Redirectview,Shorturldetailview
+urlpatterns=[
+    path("shorten/",Shorturlview.as_view(),name="shorten-url"),
+    path("<str:short_code>/",Redirectview.as_view(),name="redirect"),
+    path("shorten/<int:id>/",Shorturldetailview.as_view()),
+]
--- shortener/views.py
+++ shortener/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Shorturl
 from .serializers import ShorturlSerializer
+from django.shortcuts import get_object_or_404,redirect
 import random
 import string
 # Create your views here.
@@ -17,3 +18,31 @@ class Shorturlview(APIView):
             serializer.save()
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def get(self,request):
+        urls=Shorturl.objects.all()
+        serializer=ShorturlSerializer(urls,many=True)
+        return Response(serializer.data)
+class Redirectview(APIView):
+    def get(self,request,short_code):
+        url=get_object_or_404(Shorturl,short_code=short_code)
+        url.clicks+=1
+        url.save()
+        return redirect(url.original_url)
+class Shorturldetailview(APIView):
+    def delete(self,request,id):
+        url=get_object_or_404(Shorturl,id=id)
+        url.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    def put(self,request,id):
+        url=get_object_or_404(Shorturl,id=id)
+        serializer=ShorturlSerializer(
+            url,data=request.data,partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def get(self,request,id):
+        url=get_object_or_404(Shorturl,id=id)
+        serializer=ShorturlSerializer(url)
+        return Response(serializer.data)
